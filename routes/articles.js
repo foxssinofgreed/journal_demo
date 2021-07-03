f5f41e1b1d6def92bb95db898ac3d2e859bb76ca
fox <l_aptsiauri@cu.edu.ge>
2021-07-03 07:49:36 +0400

    Articles && New Articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+
+router.get('/', (req, res) => {
+    var articles = [{
+        title: 'test article',
+        createdAt: new Date(),
+        description: 'test description'
+    }];
+    res.render('articles/articles', {   articles: articles  });
+});
+
+router.get('/new', (req, res) => {
+    res.render('articles/new', {});
+});
+
+router.post('/', (req, res) => {
+
 });
 
 module.exports = router;
